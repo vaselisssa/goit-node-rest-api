94a874b4e24fed0ab83b4a04fb9d4bb9cc50e1c5
Svitlana Vasylkovska <vaselisssa@gmail.com>
2024-01-30 13:05:03 +0200

    add services and schemas

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,23 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+   name: Joi.string()
+      .max(50)
+      .required()
+      .messages({ "any.required": "Missing required name field" }),
+   email: Joi.string()
+      .email()
+      .required()
+      .messages({ "any.required": "Missing required email field" }),
+   phone: Joi.string()
+      .min(3)
+      .max(50)
+      .required()
+      .messages({ "any.required": "Missing required phone field" }),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+   name: Joi.string().max(50),
+   email: Joi.string().email(),
+   phone: Joi.string().min(3).max(50),
+});
---
+++ services/contactsServices.js
@@ -0,0 +1,61 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+//Повертає масив контактів
+export const listContacts = async () => {
+   const data = await fs.readFile(contactsPath, "utf-8");
+   return JSON.parse(data);
+};
+
+//Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+export const getContactById = async (contactId) => {
+   const contacts = await listContacts();
+   const result = contacts.find((item) => item.id === contactId);
+   return result || null;
+};
+
+//Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+export const removeContact = async (contactId) => {
+   const contacts = await listContacts();
+   const index = contacts.findIndex((contact) => contact.id === contactId);
+   if (index === -1) {
+      console.log("No contacts found");
+      return null;
+   }
+   const [result] = contacts.splice(index, 1);
+   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+   return result;
+};
+
+//Повертає об'єкт доданого контакту (з id).
+export const addContact = async (name, email, phone) => {
+   const contacts = await listContacts();
+
+   const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+   };
+   contacts.push(newContact);
+   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+   return newContact;
+};
+
+export const updateById = async (contactId, data) => {
+   const contacts = await listContacts();
+
+   const index = contacts.findIndex((contact) => contact.id === contactId);
+   if (index === -1) {
+      console.log("No contacts found");
+      return null;
+   }
+
+   const contactToUpdate = contacts.find((contact) => contact.id === contactId);
+   contacts[index] = { id, ...contactToUpdate, ...data };
+   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+   return contacts[index];
+};
