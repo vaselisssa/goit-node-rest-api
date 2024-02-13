94bce47411b4e9eee337849a8a514cc277ef936f
Svitlana Vasylkovska <vaselisssa@gmail.com>
2024-02-13 18:48:28 +0200

    add login controller, route

---
+++ controllers/auth/login.js
@@ -0,0 +1,40 @@
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import dotenv from "dotenv";
+import { User } from "../../models/index.js";
+import { HttpError } from "../../helpers/index.js";
+
+dotenv.config();
+const { SECRET_KEY } = process.env;
+
+export const login = asyncHandler(async (req, res) => {
+   const { email, password } = req.body;
+
+   const user = await User.findOne({ email });
+
+   if (!user) {
+      throw HttpError(401, "Email or password is wrong");
+   }
+
+   const isValidPassword = await bcryptjs.compare(password, user.password);
+
+   if (!isValidPassword) {
+      throw HttpError(401, "Email or password is wrong");
+   }
+
+   const payload = {
+      id: user._id,
+   };
+
+   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+   await User.findByIdAndUpdate(user._id, { token });
+
+   res.status(200).json({
+      token,
+      user: {
+         email: user.email,
+         subscription: user.subscription,
+      },
+   });
+});
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { register } from "../controllers/auth/index.js";
+import { register, login } from "../controllers/auth/index.js";
 import { authSchemas } from "../schemas/index.js";
 import { validateBody } from "../midlewares/index.js";
 
-const { registerSchema } = authSchemas;
+const { registerSchema, loginSchema } = authSchemas;
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(registerSchema), register);
+
+authRouter.post("/login", validateBody(loginSchema), login);
 
 export { authRouter };
