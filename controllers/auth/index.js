4098fc57976a4ff94f40a348e2f4bf1fec0030e4
Svitlana Vasylkovska <vaselisssa@gmail.com>
2024-02-15 07:47:27 +0200

    add logout, updateSubscription

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,3 +1,5 @@
 export { register } from "./register.js";
 export { login } from "./login.js";
 export { getCurrent } from "./getCurrent.js";
+export { logout } from "./logout.js";
+export { updateSubscription } from "./updateSubscription.js";
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { register, login, getCurrent } from "../controllers/auth/index.js";
+import {
+   register,
+   login,
+   getCurrent,
+   logout,
+   updateSubscription,
+} from "../controllers/auth/index.js";
 import { authSchemas } from "../schemas/index.js";
 import { authMiddleware, validateBody } from "../midlewares/index.js";
 
-const { registerSchema, loginSchema } = authSchemas;
+const { registerSchema, loginSchema, updateSubscriptionSchema } = authSchemas;
 
 const authRouter = express.Router();
 
@@ -12,5 +18,14 @@ authRouter.post("/register", validateBody(registerSchema), register);
 authRouter.post("/login", validateBody(loginSchema), login);
 
 authRouter.get("/current", authMiddleware, getCurrent);
+
+authRouter.post("/logout", authMiddleware, logout);
+
+authRouter.patch(
+   "/",
+   authMiddleware,
+   validateBody(updateSubscriptionSchema),
+   updateSubscription
+);
 
 export { authRouter };
